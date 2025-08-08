4220e19609170cbff441c4fb4a414257e1137fad
ansuljain789 <ansuljain789@gmail.com>
2025-08-08 11:36:58 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function App() {
-  return 
-  <>
-  <H1>Hii i ama ansul jain creating your repo to start the proj</H1>
+  return (
+    <>
+  <h1>Hii i ama ansul jain creating your repo to start the proj</h1>
   </>
+  )
+  
 
 }
 
