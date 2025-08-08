9e12c48f3cadc09f6a6df9c434583b6e45b8a355
mihirkumarmeher-123 <mihirmeher22@gmail>
2025-08-08 11:44:47 +0530

    added h2 tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import React from "react";
 function App() {
   return (
     <>
-  <h1>Hii i ama ansul jain creating your repo to start the proj</h1>
-  </>
-  )
-  
-
+      <h1>Hii i ama ansul jain creating your repo to start the proj</h1>
+      <h2>This is a sub heading</h2>
+    </>
+  );
 }
 
 export default App;
