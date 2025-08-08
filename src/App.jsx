c710ec7c69fea761442407e0add641bd75d25231
mihirkumarmeher-123 <mihirmeher22@gmail>
2025-08-08 12:00:27 +0530

    gijg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <h1>Hii i ama ansul jain creating your repo to start the proj</h1>
       <h2>This is a sub heading</h2>
-      <p>This is a paragraph of text.</p>
+      
       <p>This is another paragraph of text.</p>
       <p>gijg</p>
       <p>yfugihppokp</p> 
